14f370ed9cde3537807d0eaaf744c870e538b016
shannonhickman2004 <shannonhickman2004@gmail.com>
2025-03-15 16:29:36 -0600

    final week 2

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -7,8 +7,8 @@ function productCardTemplate(product) {
   return `<li class="product-card">
     <a href="product_pages/index.html?product=${product.Id}">
       <img src="${product.Image}" alt="Image of ${product.Name}" />
-      <h3 class="card__brand">${product.Brand.Name}</h3>
       <h2 class="card__name">${product.Name}</h2>
+      <h3 class="card__brand">${product.Brand.Name}</h3>
     </a>
     <div class="price-container">
       ${hasDiscount ? `<span class="discount-badge">${discount}% OFF</span>` : ""}
