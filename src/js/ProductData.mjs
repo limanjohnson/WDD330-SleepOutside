5b08b1c18dbaf4fab2df89613bd2ba87fcc74f5e
shannonhickman2004 <shannonhickman2004@gmail.com>
2025-03-25 21:36:56 -0600

    week4

--- src/js/ProductData.mjs
+++ src/js/ProductData.mjs
@@ -23,4 +23,15 @@ export default class ProductData {
     console.log(data.Result);
     return data.Result;
   }
+  async checkout(payload) {
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(payload),
+    };
+    return await fetch(`${baseURL}checkout/`, options).then(convertToJson);
+  }
 }
+
