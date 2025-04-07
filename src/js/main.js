e89e3ec61dad431245211ffcada4c8b34754fa16
dadulisek <navratildada@gmail.com>
2025-04-07 20:33:32 +0200

    seems to be all done

--- src/js/main.js
+++ src/js/main.js
@@ -6,3 +6,22 @@ updateCartCount();
 
 // load header and footer
 loadHeaderFooter();
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const banner = document.getElementById('registrationBanner');
+    const closeButton = document.getElementById('closeBanner');
+    const bannerShown = localStorage.getItem('bannerShown');
+  
+    // Check if the banner has already been shown
+    if (!bannerShown) {
+      // If not shown, display the banner
+      banner.style.display = 'block';
+  
+      // When the user clicks the close button, hide the banner and remember
+      closeButton.addEventListener('click', function() {
+        banner.style.display = 'none';
+        localStorage.setItem('bannerShown', 'true'); // Remember we showed it
+      });
+    }
+  });
